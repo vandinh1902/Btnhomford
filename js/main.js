4e5298df66c758e0293c88f59197f1d26bc4be18
Dinh <ngovandinh190298@gmail.com>
2021-03-22 18:02:53 +0700

    D

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Menu mobile
 const menuIcon = document.querySelector(".nav_hamburger_menu");
 const hambergerBtn = document.querySelector(".hamburger_btn")
 const menuText = document.querySelector(".menu_text");
@@ -7,3 +8,15 @@ menuIcon.addEventListener("click", () => {
     hambergerBtn.classList.toggle("change");
     
 });
+
+
+// Gallary
+const viewBtn = document.querySelector(".view-btn");
+const viewAll = document.querySelector(".view-all");
+const viewLess = document.querySelector(".view-less");
+const viewContent = document.querySelector(".list");
+viewBtn.addEventListener("click", () => {
+    viewBtn.classList.toggle("open");
+    viewContent.classList.toggle("open");
+
+})
